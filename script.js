b1373ed612632a8eddf85b50ad162b5e2e35d643
Ovitor11 <joaovitoravelinodasilva1@gmail.com>
2026-04-16 23:56:56 -0300

    Refactor event countdown and add interaction features

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
-// DATA DO EVENTO
-const dataEvento = new Date("April 20, 2026 16:00:00").getTime();
+const video = document.getElementById("videoAbertura");
+const telaVideo = document.getElementById("telaVideo");
+const telaConvite = document.getElementById("telaConvite");
+const btnSom = document.getElementById("btnSom");
 
-// CONTADOR
-setInterval(() => {
-  let agora = new Date().getTime();
-  let tempo = dataEvento - agora;
+const btnWhatsapp = document.getElementById("btnWhatsapp");
+const btnLocal = document.getElementById("btnLocal");
+const btnPresente = document.getElementById("btnPresente");
 
-  let dias = Math.floor(tempo / (1000 * 60 * 60 * 24));
-  let horas = Math.floor((tempo / (1000 * 60 * 60)) % 24);
-  let minutos = Math.floor((tempo / (1000 * 60)) % 60);
+let transicaoIniciada = false;
 
-  document.getElementById("contador").innerHTML =
-    `⏳ Faltam ${dias} dias, ${horas}h e ${minutos}m`;
-}, 1000);
+function iniciarTransicao() {
+  if (transicaoIniciada) return;
+  transicaoIniciada = true;
 
-// BOTÃO
-function confirmar() {
-  alert("🐒 Presença confirmada! Te esperamos no safari do Gael!");
+  telaConvite.classList.remove("escondido");
+
+  requestAnimationFrame(() => {
+    telaConvite.classList.add("mostrar");
+    telaVideo.classList.add("sumindo");
+  });
+
+  setTimeout(() => {
+    video.pause();
+    telaVideo.style.display = "none";
+    document.body.style.overflow = "auto";
+  }, 1000);
 }
+
+btnSom.addEventListener("click", () => {
+  video.muted = false;
+  video.play();
+  btnSom.style.display = "none";
+});
+
+video.addEventListener("timeupdate", () => {
+  if (!video.duration || transicaoIniciada) return;
+
+  const tempoRestante = video.duration - video.currentTime;
+
+  if (tempoRestante <= 0.6) {
+    iniciarTransicao();
+  }
+});
+
+video.addEventListener("ended", iniciarTransicao);
+
+btnWhatsapp.addEventListener("click", () => {
+  const numero = "558198332042";
+  const texto = "Olá! Confirmo presença no aniversário do Gael 🦁🎉";
+  const link = `https://wa.me/${numero}?text=${encodeURIComponent(texto)}`;
+
+  window.open(link, "_blank");
+});
+
+btnLocal.addEventListener("click", () => {
+  const linkMaps = "https://maps.app.goo.gl/dZK3eFPxo3Fr3zGv5?g_st=aw";
+  window.open(linkMaps, "_blank");
+});
+
+btnPresente.addEventListener("click", () => {
+  alert("Sugestões de presentes: roupinhas, brinquedos educativos ou um mimo especial para o Gael 🎁");
+});
